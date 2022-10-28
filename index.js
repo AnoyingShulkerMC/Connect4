99b2d15e8e68dc21ba31a7e7951f8415a6a2d24f
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2022-10-27 20:57:26 -0700

    cli moment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import chalk from "chalk"
 var publicSessions = []
 import Board from "./lib/Board.js"
+import keypress from "keypress"
+var softdrop = false
 var colors = [
   chalk.inverse.white,
   chalk.inverse.cyan,
@@ -20,17 +22,46 @@ Board.prototype.toString = function () {
         this.piece.x <= col && col < this.piece.x + this.piece.tetro.length &&
         (this.board.length - this.piece.y - 1) <= row && row < (this.board.length - this.piece.y - 1) + this.piece.tetro.length && 
          this.piece.tetro[row - (this.board.length - this.piece.y - 1)][col - this.piece.x] !== 0) {
-        ret += colors[this.piece.tetro[row - (this.board.length - this.piece.y - 1)][col - this.piece.x]](" ")
+        ret += colors[this.piece.tetro[row - (this.board.length - this.piece.y - 1)][col - this.piece.x]]("  ")
         continue
       }
-      ret += colors[this.board[row][col]](" ")
+      ret += colors[this.board[row][col]]("  ")
     }
-    ret += (row == this.length - 1 ? "\n" + "-".repeat(this.width) : "") + "\n"
+    ret += (row == this.length - 1 ? "\n" + "--".repeat(this.width) : "") + "\n"
   }
   return ret
 }
 var board = new Board(20, 10)
 board.nextPiece()
-for(var i = 0; i < 10; i ++)board.piece.hardDrop()
-console.log(board.piece)
+board.board[39][3] = 8
+board.setLevel(1)
+keypress(process.stdin)
+process.stdin.setRawMode(true)
+process.stdin.on("keypress", (_, key) => {
+
+  switch (key.name) {
+    case "left":
+      board.piece.shift(-1)
+      break;
+    case "right":
+      board.piece.shift(1)
+      break;
+    case "space":
+      board.hardDrop()
+      break;
+    case "z":
+      board.rotCCW()
+      break;
+    case "x":
+      board.rotCC()
+  }
+  if (key.name == "c" && key.ctrl) process.exit()
+})
+setInterval(() => {
+  board.update(500)
+  console.clear()
+  let a = []
+  for (let i = 0; i < board.next.length; i++)a.push(board.next[i].name)
+  console.log(board.toString() + "Next: " + a.join(","))
+}, 500)
 console.log(board.toString())
