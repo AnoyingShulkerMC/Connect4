554caad512ad3729786e71ad868bd220a9b1ec50
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2022-10-27 22:39:48 -0700

    a

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var colors = [
 Board.prototype.toString = function () {
   var ret = ""
   for (var row = 0; row < this.board.length; row++) {
+    if(row < board.length - 1) continue
     for (var col = 0; col < this.board[row].length; col++) {
       if (this.piece &&
         this.piece.x <= col && col < this.piece.x + this.piece.tetro.length &&
@@ -33,7 +34,6 @@ Board.prototype.toString = function () {
 }
 var board = new Board(20, 10)
 board.nextPiece()
-board.board[39][3] = 8
 board.setLevel(1)
 keypress(process.stdin)
 process.stdin.setRawMode(true)
@@ -46,6 +46,9 @@ process.stdin.on("keypress", (_, key) => {
     case "right":
       board.piece.shift(1)
       break;
+    case "down":
+      softdrop = true
+      break
     case "space":
       board.hardDrop()
       break;
@@ -54,14 +57,22 @@ process.stdin.on("keypress", (_, key) => {
       break;
     case "x":
       board.rotCC()
+      break;
+    case "c":
+      board.holdPiece()
+      break;
+    case "r":
+      board = new Board(20, 10)
+      board.nextPiece()
+      board.setLevel(1)
   }
   if (key.name == "c" && key.ctrl) process.exit()
 })
 setInterval(() => {
-  board.update(500)
+  board.update(100, softdrop)
+  softdrop = false
   console.clear()
   let a = []
   for (let i = 0; i < board.next.length; i++)a.push(board.next[i].name)
-  console.log(board.toString() + "Next: " + a.join(","))
-}, 500)
-console.log(board.toString())
+  console.log(board.toString() + "Next: " + a.join(",") + "\nHold: " + (board.hold == null ? "N/A" : board.hold.name) + "\nMoves Left: " + board.piece.moves + "\nLevel: " + board.level)
+}, 100)
